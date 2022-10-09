72619fd3639afb352d77b1b531133e7bed1806f8
gtaggana <AYGlazkov@stud.kpfu.ru>
2022-10-09 20:04:40 +0300

    wordpress :white_check_mark:

--- srcs/requirements/mariadb/tools/schema.sh
+++ srcs/requirements/mariadb/tools/schema.sh
@@ -2,18 +2,20 @@
 
 /etc/init.d/mariadb setup
 rc-service mariadb start
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
 echo "CREATE DATABASE ${DB_NAME};
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASS}';
 CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;" > /tmp/schema.sql
 cat /tmp/schema.sql
+mysql -u root -proot < /tmp/schema.sql
+
+fi
 
 sed -i "s|skip-networking|skip-networking=0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-mysql -u root < /tmp/schema.sql
 
 rc-service mariadb restart
 rc-service mariadb stop
 
-/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
+/usr/bin/mariadbd --datadir=/var/lib/mysql --user=mysql --pid-file=/run/mysqld/mariadb.pid
--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,16 +1,23 @@
 #!bin/sh
+if [ ! -f "/var/www/wp-config.php" ]; then
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+chmod +x wp-cli.phar;
+mv wp-cli.phar /usr/local/bin/wp;
+
+wp core download --allow-root;
 
 cat << EOF > /var/www/wp-config.php
 <?php
-define('DB_NAME', '${DB_NAME}');
-define('DB_USER', '${DB_USER}');
-define('DB_PASSWORD', '${DB_PASS}' );
-define('DB_HOST', '${DB_HOST}' );
-define('DB_CHARSET', 'utf8' );
-define('DB_COLLATE', '' );
+define( 'DB_NAME', '${DB_NAME}' );
+define( 'DB_USER', '${DB_USER}' );
+define( 'DB_PASSWORD', '${DB_PASS}' );
+define( 'DB_HOST', 'mariadb' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
 \$table_prefix = 'wp_';
-define( 'WP_DEBUG', false);
+define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 define( 'WP_REDIS_HOST', 'redis' );
@@ -19,6 +26,16 @@ define( 'WP_REDIS_TIMEOUT', 1 );
 define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 require_once ABSPATH . 'wp-settings.php';
-#iiiiiiiiiiii########
 EOF
 
+wp core install --url=${DOMAIN_NAME} --title=Inception --admin_user=${DB_ROOT} --admin_password=${DB_PASSW} --admin_email=${DB_ROOT_MAIL} --allow-root
+wp user create ${DB_USER} ${DB_USER_MAIL} --allow-root --role=subscriber --user_pass=${DB_PASSW}
+
+wp plugin install redis-cache --activate --allow-root
+wp plugin update --all --allow-root
+
+wp config set WP_REDIS_HOST "redis"
+wp config set WP_REDIS_PORT "6379"
+fi
+
+/usr/sbin/php-fpm8 -F
